e7ac87676cdad95ee2231907569a893817af7908
alexdecycive <alex@decycive.com>
2017-12-05 12:25:40 -0500

    final package updates

---
+++ app/models/indexDEV.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const basename  = path.basename(__filename);
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('saaatsmini', 'root', 'Test101!', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorsAliases: false
+});
+const db = {};
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        let  model = sequelize['import'](path.join(__dirname, file));
+
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
